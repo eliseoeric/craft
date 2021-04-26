dc00d6d6a1daa9eb4c7bf4c99e272dbca55f3cc0
Austin Herman <austin.j.herman@gmail.com>
2021-04-26 09:52:45 -0400

    begin footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql, useStaticQuery } from 'gatsby'
 import cx from 'classnames'
 import Container from '@Components/Grid/Container'
 import Row from '@Components/Grid/Row'
 import Logo from '@Components/Logo'
-import Widget from '@Components/Widget'
-import WidgetList from '@Components/Widget/List'
-import WidgetSubscribe from '@Components/Widget/Subscribe'
-import WidgetMap from '@Components/Widget/Map'
+import Clock from '@Components/Clock'
+// import WidgetSubscribe from '@Components/Widget/Subscribe'
+// import WidgetMap from '@Components/Widget/Map'
 import SocialIcons from '@Components/SocialIcons'
 import * as footer from './footer.module.scss'
 
@@ -17,30 +14,27 @@ const Footer = ({ className }) => {
 
   return (
     <footer className={cx(className, footer.site__footer)}>
+
+      <div className={cx(footer.footer__logo_wrap)}>
+        <Logo variant={'gray'} title={'Craft'} />
+      </div>
+
+      <div className={cx(footer.footer__clock_wrap)}>
+        <Clock location="Philly" />
+      </div>
+
+
       <Container>
         <Row>
-          <Logo variant={'footer'} title={'Kitch Meals'} />
+          
         </Row>
 
         <Row justifyContent={'between'}>
           <div className={footer.footer__nav}>
-            
-            <Widget title={'Stay Updated'} wide>
-              <WidgetSubscribe />
-            </Widget>
             <SocialIcons />
           </div>
-
-          <Widget className={footer.store_map}>
-            <WidgetMap />
-          </Widget>
         </Row>
 
-        <Row>
-          <p className={footer.copyright}>
-            &copy; Copyright {new Date().getFullYear()} Kitch
-          </p>
-        </Row>
       </Container>
     </footer>
   )
