6b092f5a3d11dffd55ca1a490aeaa461a414e37d
Eric Eliseo <eric.eliseo@gmail.com>
2021-07-05 22:34:19 -0500

    Fix case study slider width
    This commit fixes an issue with the slider width on the case studies,
    where the width was 10,000 + pixels, due to a flexbox calculation
    error.

--- client/src/modules/Carousel/index.jsx
+++ client/src/modules/Carousel/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { graphql } from 'gatsby'
 import uuid from 'uuid'
 
@@ -8,13 +8,27 @@ import Slide from '@Components/Slider/Slide'
 import * as styles from './styles.module.scss'
 
 const CarouselModule = ({ title, slug, slides }) => {
+  const carouselRef = useRef(null)
+  const [height, setHeight] = useState(0)
+  // No need to render during SSR
   if (!isBrowser) {
-    return null;
+    return null
   }
+
+  /**
+   * Set the height of the carousel wrapper to match the inner slick-track height
+   */
+  const onReInit = () => {
+    if (carouselRef.current) {
+      const sliderNode = carouselRef.current.innerSlider.track.node
+      setHeight(sliderNode.clientHeight)
+    }
+  }
+
   return (
-    <section id={slug}>
+    <section id={slug} className={styles.carousel__wrapper} style={{height: `${height}px`}}>
       <div className={styles.container}>
-        <Slider className={'carousel'}>
+        <Slider className={'carousel'} ref={carouselRef} onReInit={onReInit}>
           {slides &&
             slides.map(({ title, description, media }, index) => {
               return (
